1bca6f4d52a3063c65d28e93627d8bae51dc42ff
Craig Neff <ifly12610@gmail.com>
2021-02-24 01:27:00 -0700

    created a menu and messed around with that

--- practiceWithVideos.js
+++ practiceWithVideos.js
@@ -74,7 +74,7 @@ class CollectionsSender extends NotificationSender {
 
 let collection1 = new CollectionsSender("OVERDUE");
 collection1.sendNotification(`This is a test collection notification`);
-*/
+
 // Try, Catch and finally. Error handeling.
 try {
   list.push("Hello");
@@ -84,3 +84,90 @@ try {
 }
 
 console.log("goodbye");
+*/
+
+class Player {
+  constructor(name, position) {
+    this.name = name;
+    this.position = position;
+  }
+
+  describe() {
+    return `${this.name} players ${this.position}`;
+  }
+}
+
+class Team {
+  constructor(name) {
+    this.name = name;
+    this.players = [];
+  }
+
+  addPlayer(player) {
+    if (player instanceof Player) {
+      this.players.push(player);
+    } else {
+      throw new Error(
+        `You can only add instance of Player. Argument is not a player: ${player}`
+      );
+    }
+  }
+  describe() {
+    return `${this.name} has ${this.players.length} players.`;
+  }
+}
+
+class Menu {
+  constructor() {
+    this.teams = [];
+    this.selected = null;
+  }
+  start() {
+    let selection = this.showMainMenuOptions();
+    while (selection != 0) {
+      switch (selection) {
+        case "1":
+          this.createTeam();
+          break;
+        case "2":
+          this.viewTeam();
+          break;
+        case "3":
+          this.deleteTeam();
+          break;
+        case "4":
+          this.displayTeams();
+          break;
+        default:
+          selection = 0;
+      }
+      selection = this.showMainMenuOptions();
+    }
+    alert(`Goodbye!`);
+  }
+  showMainMenuOptions() {
+    return prompt(`
+        0) exit
+        1) create new team
+        2) view team
+        3) delete team
+        4) display all teams
+      `);
+  }
+  displayTeams() {
+    let teamString = "";
+    for (let i = 0; i < this.teams.length; i++) {
+      teamString += i + ") " + this.teams[i].name + " \n";
+    }
+    alert(teamString);
+  }
+  createTeam() {
+    let name = prompt(`Enter name for new team`);
+    this.teams.push(new Team(name));
+  }
+
+  viewTeam() {
+      let index = prompt('Enter the index of the team you wish to view')
+      if (index > -1)
+  }
+}
