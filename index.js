aeeafec9a5628d5604c67890dd0b876e4d3c6680
Craig Neff <ifly12610@gmail.com>
2021-02-26 19:10:58 -0700

    started coding out this weeks execerise

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
 "use strict";
-sdsdsdsdsd;
+
+//this app takes is for stocking and unstocking a vitural candy-store shelf that can only hold 10 products before needing to add another shelf.
+
+// 4 types of candy. 1) Hard 2) Chocolate
+// 3)Gummy  4) Beverage
+
+class CandyProduct {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
+  describe() {
+    console.log(
+      `This candy is, ${this.name}. ${this.name} is a ${this.type}type of candy`
+    );
+    return this.name, this.type;
+  }
+}
+
+let skittles = new CandyProduct("skittles", "hard candy");
+
+//If you are creating a new shelf you need to put at least 1 item on it. Or you shouldn't be creating a new shelf.
+
+class TenItemShelf {
+  constructor(CandyProduct) {
+    this.heldItems = [newItemOne];
+  }
+  itemsOnShelf() {
+    console.log(
+      `This is the items on shelf function for saying how many items are on the shelf.`
+    );
+  }
+}
+
+skittles.describe();
+
+class Menu {
+  constructor() {}
+}
